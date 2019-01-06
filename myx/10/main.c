d85f6524d63b9503237af6e08dae444a1bfaf3f0
EthanMeng2003 <1738678304@qq.com>
2019-01-06 11:49:20 +0800

    10

--- myx/10/main.c
+++ myx/10/main.c
@@ -13,39 +13,42 @@ typedef struct Stack
     char data[STACK_SIZE];
 }stacktype;
 
-void InitStack(struct Stack *s)
+void InitStack(stacktype **s)
 {
-	s = (struct Stack*)malloc(sizeof(struct Stack));
-	s->top = -1;
-	s->length = STACK_SIZE;
+	stacktype *p;
+	p = (stacktype*)malloc(sizeof(stacktype));
+	p->top = -1;
+	p->length = STACK_SIZE;
+	memset(p->data, 0, STACK_SIZE);
+	*s = p;
 }
 
-int StackEmpty(struct Stack *s)
+int StackEmpty(stacktype *s)
 {
 	if(s->top==-1)
     	return 1;
     return 0;
 }
 
-int StackFull(struct Stack *s)
+int StackFull(stacktype *s)
 {
     if(s->length==s->top+1)
     	return 1;
     return 0;
 }
 
-void Push(int x,struct Stack *s)
+void Push(int x,stacktype *s)
 {
     if(StackFull(s))
     {
-        printf("Full struct Stack!\n");
+        printf("Full stacktype!\n");
         return;
     }
     else
         s->data[++s->top] = x;
 }
 
-void Pop(struct Stack *s)
+void Pop(stacktype *s)
 {
     if(StackEmpty(s))
     {
@@ -56,12 +59,12 @@ void Pop(struct Stack *s)
         s->top--;
 }
 
-void FreeStack(struct Stack *s)
+void FreeStack(stacktype *s)
 {
 	free(s);
 }
 
-int GetTop(struct Stack *s)
+int GetTop(stacktype *s)
 {
 	if(s->top!=-1)
 		return s->data[s->top];
@@ -70,29 +73,37 @@ int GetTop(struct Stack *s)
 
 int fun(char str[],int length)
 {
-	struct Stack *a,*b,*c;
-	int i=0,e;
-	InitStack(a);
-	InitStack(b);
-	InitStack(c);
-	while(i<length&&!StackEmpty(a)&&!StackEmpty(b)&&!StackEmpty)
+	stacktype *a;
+	int i=0;
+	char e;
+	InitStack(&a);
+	while(i<length)
 	{
-		if(str[i]=='('||str[i]=='['||str[i]=='{')
-			Push(str[i],a);
-		if(StackEmpty(a)==0)
-		{
-			e=GetTop(a);
-			if(e==str[i])
-			{
+		if(str[i] == '(')
+			Push(str[i], a);
+		else if(str[i] == '[')
+			Push(str[i], a);
+		else if(str[i] == '{')
+			Push(str[i], a);
+		else if(str[i] == ')')
+			if (GetTop(a) == '(' && StackEmpty(a) == 0)
 				Pop(a);
-			}
-		}
-		else
-			return 0;
+			else
+				return 0;
+		else if(str[i] == ']')
+			if (GetTop(a) == '[' && StackEmpty(a) == 0)
+				Pop(a);
+			else
+				return 0;
+		else if(str[i] == '}')
+			if (GetTop(a) == '{' && StackEmpty(a) == 0)
+				Pop(a);
+			else
+				return 0;
 		i++;
 	}
 	if(StackEmpty(a)==0)
-		return 0; 
+		return 1; 
 	FreeStack(a);
 	return 1;
 }
