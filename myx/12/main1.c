495a980ed7c875cb3b3f7d05aa85ae33c42cdc29
harrisonmeng <harrisonmeng@msn.com>
2019-01-07 22:54:47 +0800

    upload exercise 12

---
+++ myx/12/main1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define Maxsize 20
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+typedef struct Queue
+{
+	char data[Maxsize];
+	int front;
+	int rear;
+}queuetype;
+
+void InitQueue(struct Queue *q)
+{
+	q=(struct Queue*)malloc(sizeof(struct Queue));
+	q->front=q->rear=0;
+}
+
+void FreeQueue(struct Queue *q)
+{
+	free(q);
+}
+
+int QueueEmpty(struct Queue *q)
+{
+	return (q->front==q->rear);
+}
+
+int enQueue(struct Queue *q,int i)
+{
+	if((q->rear+1)%Maxsize==q->front)
+		return 0;
+	q->rear=(q->rear+1)%Maxsize;
+	q->data[q->rear]=i;
+	return 1;
+}
+
+char deQueue(struct Queue *q)
+{
+	if(q->front==q->rear)
+		return 0;
+	q->front=(q->front+1)%Maxsize;
+	return q->data[q->front];
+}
+
+void fun(struct Queue *q,int length)
+{
+	int i,j,temp;
+	for(i=0,j=length-1;i<j;++i,--j)
+	{
+		temp=q->data[i];
+		q->data[i]=q->data[j];
+		q->data[j]=temp;
+	} 
+}
+int main(int argc, char** argv) {
+	struct Queue *q;
+	int i=0;
+	q=(struct Queue*)malloc(sizeof(struct Queue));
+	for(i=0;i<10;i++)
+		q->data[i]='a'+i; 
+	q->front=9;
+	q->rear=0;
+	for(i=0;i<q->front+1;i++)
+	{
+		printf("%c,",q->data[i]);
+	}
+	printf("\n");
+	fun(q,q->front+1);
+	i=0;
+	for(i=0;i<q->front+1;i++)
+	{
+		printf("%c,",q->data[i]);
+	}
+	FreeQueue(q);
+	return 0;
+}
