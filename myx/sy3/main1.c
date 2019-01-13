c79661bd5cda0cf1ac8b3189e385cd26f8b0ed27
harrisonmeng <harrisonmeng@msn.com>
2019-01-13 22:11:46 +0800

    upload sy3
    实验3：实现环形队列的各种基本运算

---
+++ myx/sy3/main1.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define Maxsize 20
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+//实现环形队列的各种基本运算的算法
+//实现环形队列（元素类型为char）的各种基本运算，并在此基础上设计一个程序，完成以下功能： 
+//1.初始化队列q
+//2.判断队列q是否非空
+//3.依次进队元素a, b, c
+//4.出队一个元素，输出该元素
+//5.依次进队元素d, e, f
+//6.输出出队序列
+//7.释放队列 
+
+typedef struct Queue
+{
+	char data[Maxsize];
+	int front,rear;
+};
+
+void InitQueue(struct Queue *q)
+{
+	q=(struct Queue*)malloc(sizeof(struct Queue));
+	q->front=q->rear=0;
+}
+
+void FreeQueue(struct Queue *q)
+{
+	free(q);
+}
+
+int QueueEmpty(struct Queue *q)
+{
+	return (q->front==q->rear);
+}
+
+int enQueue(struct Queue *q,char i)
+{
+	if((q->rear+1)%Maxsize==q->front)
+		return 0;
+	q->rear=(q->rear+1)%Maxsize;
+	q->data[q->rear]=i;
+	return 1;
+}
+
+char deQueue(struct Queue *q)
+{
+	int e;
+	if(q->front==q->rear)
+		return 0;
+	q->front=(q->front+1)%Maxsize;
+	return q->data[q->front];
+}
+
+void OutQueue(struct Queue *q)
+{
+	while(QueueEmpty(q)==0)
+		printf("%c ",deQueue(q));
+}
+
+int main(int argc, char** argv) {
+	struct Queue *q;
+	q=(struct Queue*)malloc(sizeof(struct Queue));
+	q->front=q->rear=0;
+	InitQueue(q);
+	printf("%d\n",QueueEmpty(q));
+	enQueue(q,'a');
+	enQueue(q,'b');
+	enQueue(q,'c');
+	printf("%c\n",deQueue(q));
+	enQueue(q,'d');
+	enQueue(q,'e');
+	enQueue(q,'f');
+	OutQueue(q);
+	FreeQueue(q);
+	return 0;
+}
