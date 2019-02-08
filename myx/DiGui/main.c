360dfd7fe85c53bf63dc3fecd21bfc8dd6f9dbce
EthanMeng2003 <1738678304@qq.com>
2019-02-08 23:07:32 +0800

    digui

--- myx/DiGui/main.c
+++ myx/DiGui/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float fun(int a[], int length)
+float fun(float a[], int length)
 {
     if(length == 0)
         return a[length];
     else
-        return (a[length-1]+(length-1)*fun(a, length - 1)) / length;
+        return (a[length - 1] + (length - 1) * fun(a, length - 1)) / length;
 }
 
 int main()
 {
-    int a[] = {1,2,1,2,1,2};
-    int length = 6;
+    float a[] = {1,2,3,4,5,6};
+    int length = sizeof(a) / sizeof(float);
     printf("%f", fun(a, length));
     return 0;
 }
