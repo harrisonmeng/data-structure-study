0075dd50e50105264072110b508e3ac861d2e748
EthanMeng2003 <1738678304@qq.com>
2019-02-09 14:55:15 +0800

    dg3

--- myx/dg3/main.c
+++ myx/dg3/main.c
@@ -3,10 +3,12 @@
 
 int step(int n)
 {
-    if(n == 1)
+    if(n == 0)
+        return 0;
+    else if(n == 1)
         return 1;
-    else if(n == 0)
-        return 1;
+    else if(n == 2)
+        return 2;
     else
         return step(n - 1) + step(n - 2);
 }
