ef6deb8a7212a4b4e298d14ca9a281fb2f135301
harrisonmeng <harrisonmeng@msn.com>
2019-07-11 17:54:58 +0800

    tests of ArrayGT

---
+++ myx/Array_GT/GTCompare/main.c
@@ -0,0 +1,16 @@
+//
+//把下面两个广义表用单链表存储，然后用递归方式遍历并且比较两个表，打印出不同的地方
+//A( 'a', 'b', 'c', (1, 2, 3, (x, y, z), 4), 'd')
+//B( 'a', 'b', 'c', (1, 2, 3, (x, y, u), 4), 'd')
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
