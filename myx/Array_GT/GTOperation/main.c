38180bc2d8c343c1d143960a1940c7df77d96a78
EthanMeng2003 <1738678304@qq.com>
2019-07-12 21:01:12 +0800

    GTOperation

--- myx/Array_GT/GTOperation/main.c
+++ myx/Array_GT/GTOperation/main.c
@@ -6,8 +6,141 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct GLNode
+{
+    int tag;
+    union
+    {
+        char data;
+        struct GLNode *sublist;
+    }val;
+    struct GLNode *next;
+}GLNode;
+
+GLNode *CreateList(char *s)
+{
+    int i = 0;
+    GLNode *g;
+    char ch = *s++;
+    if(ch != '\0')
+    {
+        g = (GLNode*)malloc(sizeof(struct GLNode));
+        if(ch == '(')
+        {
+            g->tag = 1;
+            g->val.sublist = CreateList(s);
+            i++;
+            while(i != 0)
+            {
+                ch = *s++;
+                if(ch == '(')
+                    i++;
+                if(ch == ')')
+                    i--;
+            }
+        }
+        else if(ch == ')')
+        {
+            g = NULL;
+        }
+        else if(ch == '#')
+        {
+            g = NULL;
+        }
+        else
+        {
+            g->tag = 0;
+            g->val.data = ch;
+        }
+    }
+    else
+    {
+        g = NULL;
+    }
+    ch = *s++;
+    if(g != NULL)
+    {
+        if(ch == ',')
+        {
+            g->next = CreateList(s);
+        }
+        else
+        {
+            g->next = NULL;
+        }
+    }
+    return g;
+}
+
+int FindDepth(GLNode *a)
+{
+    if(a == NULL)
+    {
+        return 1;
+    }
+
+    int max,dep;
+    GLNode *p;
+
+    max = dep = 0;
+
+    for(p = a; p != NULL; p = p->next)
+    {
+        if(p->tag == 1)
+        {
+            dep = FindDepth(p->val.sublist);
+            if(dep > max)
+            {
+                max = dep;
+            }
+        }
+    }
+    return max + 1;
+}
+
+int FindLength(GLNode *a)
+{
+    int length = 0;
+    GLNode *p;
+    p = a;
+    while(p)
+    {
+        length++;
+        p = p->next;
+    }
+    return length;
+}
+
+char FindLargest(GLNode *a)
+{
+    char max1,max2;
+    if(a != NULL)
+    {
+        if(a->tag == 0)
+        {
+            max1 = FindLargest(a->next);
+            return(a->val.data > max1 ? a->val.data : max1);
+        }
+        else
+        {
+            max1 = FindLargest(a->val.sublist);
+            max2 = FindLargest(a->next);
+            return(max1 > max2 ? max1 : max2);
+        }
+    }
+    else
+        return 0;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    GLNode *a;
+    char list[] = "(b,(b,a,(#),d),((a,b),c,((#))))";
+
+    a = CreateList(list);
+
+    printf("Depth:%d\n", FindDepth(a->val.sublist));
+    printf("Length:%d\n", FindLength(a->val.sublist));
+    printf("Largest atom:%c", FindLargest(a->val.sublist));
     return 0;
 }
--- myx/Array_GT/SpiralMatrix/main.c
+++ myx/Array_GT/SpiralMatrix/main.c
@@ -10,6 +10,6 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
 }
