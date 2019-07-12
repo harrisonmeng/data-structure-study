cb35310cfdee51a045eb9366bf17f9bfb557ade8
EthanMeng2003 <1738678304@qq.com>
2019-07-12 18:57:17 +0800

    1

--- myx/Array_GT/MatrixOperation/main.c
+++ myx/Array_GT/MatrixOperation/main.c
@@ -33,10 +33,14 @@ typedef struct List
 void MatrixTran(List *a, List *b)
 {
     int i;
+    i = 0;
+    b->length = a->width;
+    b->width = a->length;
+    b->num = a->num;
     for(i = 0; i < a->num; i++)
     {
-        b->element[i].x = a->element[i].y;
         b->element[i].y = a->element[i].x;
+        b->element[i].x = a->element[i].y;
         b->element[i].data = a->element[i].data;
     }
 }
@@ -67,25 +71,105 @@ void *CreateList(int a[][SIZE], List *p)
 
 void PrintMatrix(List *A)
 {
-    int i,j,k;
-    i = j = k = 0;
+    int i,j,k,f;
+    i = j = k = f = 0;
     for(i = 0; i < SIZE; i++)
     {
         for(j = 0; j < SIZE; j++)
         {
-            if(A->element[k].x == i+1 && A->element[k].y == j+1)
+            for(k = 0; k < A->num; k++)
             {
-                printf("%d", A->element[k].data);
-                k++;
+                if(A->element[k].x == i+1 && A->element[k].y == j+1)
+                {
+                    printf("%d", A->element[k].data);
+                    f = 1;
+                    break;
+                }
             }
-            else
+            if(f == 0)
             {
                 printf("0");
             }
+            f = 0;
         }
         printf("\n");
     }
     printf("\n");
+}
+
+void MatrixAdd(List *a, List *b, List *apb)
+{
+    int i,j,k,f;
+    i = j = f = k = 0;
+    for(i = 0; i < a->num; i++)
+    {
+        apb->element[i].x = a->element[i].x;
+        apb->element[i].y = a->element[i].y;
+        apb->element[i].data = a->element[i].data;
+    }
+    for(j = 0; j < b->num; j++)
+    {
+        for(k = 0; k < a->num; k++)
+        {
+            if(a->element[k].x == b->element[j].x && a->element[k].y == b->element[j].y)
+            {
+                apb->element[k].data += b->element[j].data;
+                f = 1;
+                break;
+            }
+        }
+        if(f == 0)
+        {
+            i++;
+            apb->element[i].x = b->element[j].x;
+            apb->element[i].y = b->element[j].y;
+            apb->element[i].data = b->element[j].data;
+        }
+        f = 0;
+    }
+    apb->length = a->length;
+    apb->width = b->width;
+    apb->num = a->num > b->num ? a->num : b->num;
+}
+
+int find(List *a, int x, int y)
+{
+	int i;
+	for(i = 0; i < a->num; i++)
+    {
+		if (a->element[i].x == x && a->element[i].y == y)
+		{
+			return a->element[i].data;
+		}
+	}
+	return 0;
+}
+
+void MatrixMultiply(List *a, List *b, List *amb)
+{
+	int sum,num,i,j,k;
+	sum = num = i = j = k = 0;
+    for(i = 0; i < a->length; i++)
+    {
+        for(j = 0; j < b->width; j++)
+        {
+            sum = 0;
+            for(k = 0; k < a->width; k++)
+            {
+                sum = sum + find(a, i, k) * find(b, k, j);
+            }
+            if(sum != 0)
+            {
+                amb->element[num].data = sum;
+                amb->element[num].x = i;
+                amb->element[num].y = j;
+                num++;
+            }
+        }
+    }
+    amb->length = a->length;
+    amb->width = b->width;
+    amb->num = num;
 }
 
 int main()
@@ -104,8 +188,16 @@ int main()
     CreateList(b, B);
     PrintMatrix(A);
     PrintMatrix(B);
+
     MatrixTran(A, TA);
     PrintMatrix(TA);
+    MatrixTran(B, TB);
+    PrintMatrix(TB);
 
+    MatrixAdd(A,B,ApB);
+    PrintMatrix(ApB);
+
+    MatrixMultiply(A, B, AmB);
+    PrintMatrix(AmB);
     return 0;
 }
