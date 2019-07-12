dce64d4a3b7361f701738712d7b58ace71963914
harrisonmeng <harrisonmeng@msn.com>
2019-07-12 13:41:42 +0800

    more tests for Array and GT

---
+++ myx/Array_GT/GTOperation/main.c
@@ -0,0 +1,13 @@
+//
+//建立广义表g=(b, (b, a, (#)，d), ((a, b), c, ((#))))的链式存储结构 (#)代表空表
+//输出广义表g的长度和深度
+//输出广义表g的值最大的原子，应该是d (ascii值最大)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ myx/Array_GT/SpiralMatrix/main.c
@@ -0,0 +1,15 @@
+//编写程序用递归方法输出以下螺旋方阵（按顺时针方向旋进）
+//  1  2  3  4  5
+// 16 17 18 19  6
+// 15 24 25 20  7
+// 14 23 22 21  8
+// 13 12 11 10  9
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
